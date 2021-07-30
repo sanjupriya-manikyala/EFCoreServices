1cca31e0dbcf531239a618364ba838c7ab5f90ff
Sanjupriya Manikyala <sanjupriya.manikyala@cesltd.com>
2021-07-30 15:15:11 +0530

    Add project files.

---
+++ Models/Product.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace EFCoreServices.Models
+{
+    public partial class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public double? Price { get; set; }
+    }
+}
